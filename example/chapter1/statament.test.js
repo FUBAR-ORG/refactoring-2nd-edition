b25945f0098f6dd47c2832e50805f89238be81f0
IRONAGE-Park <cheolhyundev@gmail.com>
2023-06-02 00:20:03 +0900

    extract switch

--- example/chapter1/statament.test.js
+++ example/chapter1/statament.test.js
@@ -1,17 +1,28 @@
 const worker = require("./after-statemenet");
 
+const { invoice, plays } = worker;
+
 test("statement basic test", () => {
-  const invoice = worker.invoice;
-  const plays = worker.plays;
-
   const result = worker.statement(invoice, plays);
-
   expect(result).toBe(
     "청구 내역 (고객명: BigCo)\n" +
-    "Hamlet: $650.00 (55석)\n" +
-    "As You Like It: $580.00 (35석)\n" +
-    "Othello: $500.00 (40석)\n" +
-    "총액: $1,730.00\n" +
-    "적립 포인트: 47점\n"
+      "Hamlet: $650.00 (55석)\n" +
+      "As You Like It: $580.00 (35석)\n" +
+      "Othello: $500.00 (40석)\n" +
+      "총액: $1,730.00\n" +
+      "적립 포인트: 47점\n"
   );
+});
+
+test("extract switch", () => {
+  const audience = 30;
+
+  const play = { type: "comedy" };
+  const perf = { audience: audience };
+  const result = worker.amountFor(play, perf);
+  let thisAmount = 30000;
+  thisAmount += 10000 + 500 * (perf.audience - 20);
+  thisAmount += 300 * perf.audience;
+
+  expect(result).toBe(thisAmount);
 });
