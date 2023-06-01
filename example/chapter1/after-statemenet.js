1c60d3a25faa1d4f5a43c2cd9dad660c381e3f23
IRONAGE-Park <cheolhyundev@gmail.com>
2023-06-02 00:02:24 +0900

    setup

--- example/chapter1/after-statemenet.js
+++ example/chapter1/after-statemenet.js
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n;`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
 
@@ -65,3 +65,5 @@ const plays = {
 }
 
 console.log(statement(invoice, plays))
+
+module.exports = { statement, invoice, plays };
