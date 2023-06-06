a3fa5a2166574a8101bb1568570e22e83d08091f
greenneuron <goodev@kakao.com>
2023-06-06 20:02:19 +0900

    volumeCredits를 performance 객체의 프로퍼티로 설정

--- jigu/statement.js
+++ jigu/statement.js
@@ -8,6 +8,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(aPerformance);
     result.amount = amountFor(aPerformance);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
 
     function playFor(aPerformance) {
@@ -35,6 +36,14 @@ function statement(invoice, plays) {
       }
       return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+      let result = Math.max(aPerformance.audience - 30, 0);
+      if (aPerformance.play.type === "comedy") {
+        result += Math.floor(aPerformance.audience / 5);
+      }
+      return result;
+    }
   }
 }
 
@@ -58,7 +67,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (const perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -69,14 +78,6 @@ function renderPlainText(data, plays) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = Math.max(aPerformance.audience - 30, 0);
-    if (aPerformance.play.type === "comedy") {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
   }
 }
 
