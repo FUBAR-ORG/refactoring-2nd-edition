b0cfb3ac9d1bf988e3aef9f6a2ad0088106bd482
greenneuron <goodev@kakao.com>
2023-06-06 20:12:23 +0900

    총합을 statementData 객체로 이동

--- jigu/statement.js
+++ jigu/statement.js
@@ -2,6 +2,8 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -45,18 +47,8 @@ function statement(invoice, plays) {
       return result;
     }
   }
-}
 
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-  for (const perf of data.performances) {
-    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
-
-  function totalAmount() {
+  function totalAmount(data) {
     let result = 0;
     for (const perf of data.performances) {
       result += perf.amount;
@@ -64,13 +56,23 @@ function renderPlainText(data, plays) {
     return result;
   }
 
-  function totalVolumeCredits() {
+  function totalVolumeCredits(data) {
     let result = 0;
     for (const perf of data.performances) {
       result += perf.volumeCredits;
     }
     return result;
   }
+}
+
+function renderPlainText(data, plays) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (const perf of data.performances) {
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
