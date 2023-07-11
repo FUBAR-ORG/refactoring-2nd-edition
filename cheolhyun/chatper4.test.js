48c71fd0c28b5bbd1efd97035c85e5315461a123
IRONAGE-Park <iron@invaiz.com>
2023-07-11 23:50:33 +0900

    # ADD: Test 5 작성

--- cheolhyun/chatper4.test.js
+++ cheolhyun/chatper4.test.js
@@ -43,3 +43,22 @@ describe("province", function () {
     expect(asia.profit).toBe(292);
   });
 });
+
+describe("no producers", function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  it("shortfall", function () {
+    expect(noProducers.shortfall).toBe(30);
+  });
+  it("profit", function () {
+    expect(noProducers.profit).toBe(0);
+  });
+});
